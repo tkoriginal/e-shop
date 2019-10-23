7e7aef95a795f1056bf056c115a2ac942bc04d21
Tauqueer Khan <tauqueer.khan@gmail.com>
2019-10-22 21:45:46 -0400

    Added skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './component/Homepage'
 function App() {
   return (
     <div className="App">
-      Hello world
+      <HomePage />
     </div>
   );
 }
