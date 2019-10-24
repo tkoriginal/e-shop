34be593ec23544686431831ea10b15653ed40f81
Tauqueer Khan <tauqueer.khan@gmail.com>
2019-10-23 22:57:18 -0400

    added styling and transition;

--- src/components/menu-item/index.js
+++ src/components/menu-item/index.js
@@ -3,9 +3,10 @@ import React from 'react'
 import './styles.scss'
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div style={{ 
-    backgroundImage: `url(${imageUrl})`
-  }} className={size ? `menu-item ${size}` : 'menu-item'}>
+  <div className={size ? `menu-item ${size}` : 'menu-item'}>
+    <div style={{ 
+      backgroundImage: `url(${imageUrl})`
+    }} className='background-image'/>
     <div className='content'>
       <h1 className='title'>{title}</h1>
       <span className='subtitle'>SHOP NOW</span>
